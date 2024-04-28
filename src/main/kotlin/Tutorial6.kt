446d2291b4a7e6bae4f6122ba075124468cdea09
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 18:12:05 +0200

    Tuto6 8va parte

--- src/main/kotlin/Tutorial6.kt
+++ src/main/kotlin/Tutorial6.kt
@@ -235,74 +235,112 @@
 //}
 
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.material.Checkbox
+//import androidx.compose.material.Text
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.WindowPlacement
+//import androidx.compose.ui.window.WindowPosition
+//import androidx.compose.ui.window.application
+//import androidx.compose.ui.window.rememberWindowState
+//
+//fun main() = application {
+//    val state = rememberWindowState(placement = WindowPlacement.Maximized)
+//
+//    Window(onCloseRequest = ::exitApplication, state) {
+//        Column {
+//            Row(verticalAlignment = Alignment.CenterVertically) {
+//                Checkbox(
+//                    state.placement == WindowPlacement.Fullscreen,
+//                    {
+//                        state.placement = if (it) {
+//                            WindowPlacement.Fullscreen
+//                        } else {
+//                            WindowPlacement.Floating
+//                        }
+//                    }
+//                )
+//                Text("isFullscreen")
+//            }
+//
+//            Row(verticalAlignment = Alignment.CenterVertically) {
+//                Checkbox(
+//                    state.placement == WindowPlacement.Maximized,
+//                    {
+//                        state.placement = if (it) {
+//                            WindowPlacement.Maximized
+//                        } else {
+//                            WindowPlacement.Floating
+//                        }
+//                    }
+//                )
+//                Text("isMaximized")
+//            }
+//
+//            Row(verticalAlignment = Alignment.CenterVertically) {
+//                Checkbox(state.isMinimized, { state.isMinimized = !state.isMinimized })
+//                Text("isMinimized")
+//            }
+//
+//            Text(
+//                "Position ${state.position}",
+//                Modifier.clickable {
+//                    val position = state.position
+//                    if (position is WindowPosition.Absolute) {
+//                        state.position = position.copy(x = state.position.x + 10.dp)
+//                    }
+//                }
+//            )
+//
+//            Text(
+//                "Size ${state.size}",
+//                Modifier.clickable {
+//                    state.size = state.size.copy(width = state.size.width + 10.dp)
+//                }
+//            )
+//        }
+//    }
+//}
+
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 fun main() = application {
-    val state = rememberWindowState(placement = WindowPlacement.Maximized)
+    val state = rememberWindowState()
 
     Window(onCloseRequest = ::exitApplication, state) {
-        Column {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    state.placement == WindowPlacement.Fullscreen,
-                    {
-                        state.placement = if (it) {
-                            WindowPlacement.Fullscreen
-                        } else {
-                            WindowPlacement.Floating
-                        }
-                    }
-                )
-                Text("isFullscreen")
-            }
+        // Content
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    state.placement == WindowPlacement.Maximized,
-                    {
-                        state.placement = if (it) {
-                            WindowPlacement.Maximized
-                        } else {
-                            WindowPlacement.Floating
-                        }
-                    }
-                )
-                Text("isMaximized")
-            }
+        LaunchedEffect(state) {
+            snapshotFlow { state.size }
+                .onEach(::onWindowResize)
+                .launchIn(this)
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(state.isMinimized, { state.isMinimized = !state.isMinimized })
-                Text("isMinimized")
-            }
-
-            Text(
-                "Position ${state.position}",
-                Modifier.clickable {
-                    val position = state.position
-                    if (position is WindowPosition.Absolute) {
-                        state.position = position.copy(x = state.position.x + 10.dp)
-                    }
-                }
-            )
-
-            Text(
-                "Size ${state.size}",
-                Modifier.clickable {
-                    state.size = state.size.copy(width = state.size.width + 10.dp)
-                }
-            )
+            snapshotFlow { state.position }
+                .filter { it.isSpecified }
+                .onEach(::onWindowRelocate)
+                .launchIn(this)
         }
     }
+}
+
+private fun onWindowResize(size: DpSize) {
+    println("onWindowResize $size")
+}
+
+private fun onWindowRelocate(position: WindowPosition) {
+    println("onWindowRelocate $position")
 }
