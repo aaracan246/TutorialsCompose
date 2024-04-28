97e761f75996dafce4199b7520f413fc5694a8ec
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 22:07:11 +0200

    Tuto10 4ta parte

--- src/main/kotlin/Tutorial10.kt
+++ src/main/kotlin/Tutorial10.kt
@@ -203,66 +203,237 @@
 //}
 
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.awt.SwingPanel
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.rememberWindowState
-import java.awt.BorderLayout
-import javax.swing.JPanel
-import javax.swing.JLabel
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.width
+//import androidx.compose.material.Button
+//import androidx.compose.material.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.ui.awt.SwingPanel
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.application
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.rememberWindowState
+//import java.awt.BorderLayout
+//import javax.swing.JPanel
+//import javax.swing.JLabel
+//
+//val swingLabel = JLabel()
+//
+//fun main() = application {
+//    Window(
+//        onCloseRequest = ::exitApplication,
+//        state = rememberWindowState(width = 400.dp, height = 200.dp),
+//    ) {
+//        val clicks = remember { mutableStateOf(0) }
+//        Column(
+//            modifier = Modifier.fillMaxSize().padding(20.dp),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            SwingPanel(
+//                modifier = Modifier.fillMaxWidth().height(40.dp),
+//                factory = {
+//                    JPanel().apply {
+//                        add(swingLabel, BorderLayout.CENTER)
+//                    }
+//                },
+//                update = {
+//                    swingLabel.setText("SwingLabel Clicks: ${clicks.value}")
+//                }
+//            )
+//            Spacer(modifier = Modifier.height(40.dp))
+//            Row (
+//                modifier = Modifier.height(40.dp),
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Button(onClick = { clicks.value++ }) {
+//                    Text(text = "Increment")
+//                }
+//                Spacer(modifier = Modifier.width(20.dp))
+//                Button(onClick = { clicks.value-- }) {
+//                    Text(text = "Decrement")
+//                }
+//            }
+//        }
+//    }
+//}
 
-val swingLabel = JLabel()
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.awt.*
+import androidx.compose.ui.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.window.*
+import androidx.compose.ui.unit.*
+import java.awt.BorderLayout
+import java.awt.Canvas
+import java.awt.Dimension
+import java.awt.Insets
+import java.awt.event.*
+import javax.swing.*
+import javax.swing.border.EmptyBorder
+
+val Gray = java.awt.Color(64, 64, 64)
+val DarkGray = java.awt.Color(32, 32, 32)
+val LightGray = java.awt.Color(210, 210, 210)
+
+data class Item(
+    val text: String,
+    val icon: ImageVector,
+    val color: Color,
+    val state: MutableState<Boolean> = mutableStateOf(false)
+)
+val panelItemsList = listOf(
+    Item(text = "Person", icon = Icons.Filled.Person, color = Color(10, 232, 162)),
+    Item(text = "Favorite", icon = Icons.Filled.Favorite, color = Color(150, 232, 150)),
+    Item(text = "Search", icon = Icons.Filled.Search, color = Color(232, 10, 162)),
+    Item(text = "Settings", icon = Icons.Filled.Settings, color = Color(232, 162, 10)),
+    Item(text = "Close", icon = Icons.Filled.Close, color = Color(232, 100, 100))
+)
+val itemSize = 50.dp
+
+fun java.awt.Color.toCompose(): Color {
+    return Color(red, green, blue)
+}
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        state = rememberWindowState(width = 400.dp, height = 200.dp),
+        state = rememberWindowState(width = 500.dp, height = 500.dp),
     ) {
-        val clicks = remember { mutableStateOf(0) }
         Column(
-            modifier = Modifier.fillMaxSize().padding(20.dp),
+            modifier = Modifier.fillMaxSize().background(color = Gray.toCompose()).padding(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(text = "Compose Area", color = LightGray.toCompose())
+            Spacer(modifier = Modifier.height(40.dp))
             SwingPanel(
-                modifier = Modifier.fillMaxWidth().height(40.dp),
+                background = DarkGray.toCompose(),
+                modifier = Modifier.fillMaxSize(),
                 factory = {
-                    JPanel().apply {
-                        add(swingLabel, BorderLayout.CENTER)
+                    ComposePanel().apply {
+                        setContent {
+                            Box {
+                                SwingPanel(
+                                    modifier = Modifier.fillMaxSize(),
+                                    factory = { SwingComponent() }
+                                )
+                                Box (
+                                    modifier = Modifier.align(Alignment.TopStart)
+                                        .padding(start = 20.dp, top = 80.dp)
+                                        .background(color = DarkGray.toCompose())
+                                ) {
+                                    SwingPanel(
+                                        modifier = Modifier.size(itemSize * panelItemsList.size, itemSize),
+                                        factory = {
+                                            ComposePanel().apply {
+                                                setContent {
+                                                    ComposeOverlay()
+                                                }
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
                     }
-                },
-                update = {
-                    swingLabel.setText("SwingLabel Clicks: ${clicks.value}")
                 }
             )
-            Spacer(modifier = Modifier.height(40.dp))
-            Row (
-                modifier = Modifier.height(40.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Button(onClick = { clicks.value++ }) {
-                    Text(text = "Increment")
-                }
-                Spacer(modifier = Modifier.width(20.dp))
-                Button(onClick = { clicks.value-- }) {
-                    Text(text = "Decrement")
-                }
+        }
+    }
+}
+
+fun SwingComponent() : JPanel {
+    return JPanel().apply {
+        background = DarkGray
+        border = EmptyBorder(20, 20, 20, 20)
+        layout = BorderLayout()
+        add(
+            JLabel("TextArea Swing Component").apply {
+                foreground = LightGray
+                verticalAlignment = SwingConstants.NORTH
+                horizontalAlignment = SwingConstants.CENTER
+                preferredSize = Dimension(40, 160)
+            },
+            BorderLayout.NORTH
+        )
+        add(
+            JTextArea().apply {
+                background = LightGray
+                lineWrap = true
+                wrapStyleWord = true
+                margin = Insets(10, 10, 10, 10)
+                text = "The five boxing wizards jump quickly. " +
+                        "Crazy Fredrick bought many very exquisite opal jewels. " +
+                        "Pack my box with five dozen liquor jugs.\n" +
+                        "Cozy sphinx waves quart jug of bad milk. " +
+                        "The jay, pig, fox, zebra and my wolves quack!"
+            },
+            BorderLayout.CENTER
+        )
+    }
+}
+
+@Composable
+fun ComposeOverlay() {
+    Box(
+        modifier = Modifier.fillMaxSize().
+        background(color = DarkGray.toCompose()),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.background(
+                shape = RoundedCornerShape(4.dp),
+                color = Color.DarkGray.copy(alpha = 0.5f)
+            )
+        ) {
+            for (item in panelItemsList) {
+                SelectableItem(
+                    text = item.text,
+                    icon = item.icon,
+                    color = item.color,
+                    selected = item.state
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun SelectableItem(
+    text: String,
+    icon: ImageVector,
+    color: Color,
+    selected: MutableState<Boolean>
+) {
+    Box(
+        modifier = Modifier.size(itemSize)
+            .clickable { selected.value = !selected.value },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.alpha(if (selected.value) 1.0f else 0.5f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(modifier = Modifier.size(32.dp), imageVector = icon, contentDescription = null, tint = color)
+            Text(text = text, color = Color.White, fontSize = 10.sp)
         }
     }
 }
