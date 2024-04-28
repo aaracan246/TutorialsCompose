313b138463c544df29f294a936bab29ab7fa17d5
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 21:05:10 +0200

    Tuto6 11ra parte

--- src/main/kotlin/Tutorial6.kt
+++ src/main/kotlin/Tutorial6.kt
@@ -382,18 +382,60 @@
 //}
 
 
-import androidx.compose.ui.awt.ComposeWindow
-import java.awt.Dimension
-import javax.swing.JFrame
-import javax.swing.SwingUtilities
+//import androidx.compose.ui.awt.ComposeWindow
+//import java.awt.Dimension
+//import javax.swing.JFrame
+//import javax.swing.SwingUtilities
+//
+//fun main() = SwingUtilities.invokeLater {
+//    ComposeWindow().apply {
+//        size = Dimension(300, 300)
+//        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+//        setContent {
+//            // Content
+//        }
+//        isVisible = true
+//    }
+//}
 
-fun main() = SwingUtilities.invokeLater {
-    ComposeWindow().apply {
-        size = Dimension(300, 300)
-        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
-        setContent {
-            // Content
-        }
-        isVisible = true
-    }
-}
+
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.window.AwtWindow
+//import androidx.compose.ui.window.application
+//import java.awt.FileDialog
+//import java.awt.Frame
+//
+//fun main() = application {
+//    var isOpen by remember { mutableStateOf(true) }
+//
+//    if (isOpen) {
+//        FileDialog(
+//            onCloseRequest = {
+//                isOpen = false
+//                println("Result $it")
+//            }
+//        )
+//    }
+//}
+//
+//@Composable
+//private fun FileDialog(
+//    parent: Frame? = null,
+//    onCloseRequest: (result: String?) -> Unit
+//) = AwtWindow(
+//    create = {
+//        object : FileDialog(parent, "Choose a file", LOAD) {
+//            override fun setVisible(value: Boolean) {
+//                super.setVisible(value)
+//                if (value) {
+//                    onCloseRequest(file)
+//                }
+//            }
+//        }
+//    },
+//    dispose = FileDialog::dispose
+//)
