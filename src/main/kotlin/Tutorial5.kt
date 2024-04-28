f1520f23cdb665b8ab69ec7ba974c04426ff4acb
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 17:22:46 +0200

    Tuto5 4ta parte

--- src/main/kotlin/Tutorial5.kt
+++ src/main/kotlin/Tutorial5.kt
@@ -24,42 +24,66 @@
 //}
 
 
-import androidx.compose.foundation.ContextMenuDataProvider
+//import androidx.compose.foundation.ContextMenuDataProvider
+//import androidx.compose.foundation.ContextMenuItem
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.text.selection.SelectionContainer
+//import androidx.compose.material.Text
+//import androidx.compose.material.TextField
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.singleWindowApplication
+//
+//fun main() = singleWindowApplication(title = "Context menu") {
+//    val text = remember { mutableStateOf("Hello!") }
+//    Column {
+//        ContextMenuDataProvider(
+//            items = {
+//                listOf(
+//                    ContextMenuItem("User-defined Action") {/*do something here*/ },
+//                    ContextMenuItem("Another user-defined action") {/*do something else*/ }
+//                )
+//            }
+//        ) {
+//            TextField(
+//                value = text.value,
+//                onValueChange = { text.value = it },
+//                label = { Text(text = "Input") }
+//            )
+//
+//            Spacer(Modifier.height(16.dp))
+//
+//            SelectionContainer {
+//                Text("Hello World!")
+//            }
+//        }
+//    }
+//}
+
+
+
+import androidx.compose.foundation.ContextMenuArea
 import androidx.compose.foundation.ContextMenuItem
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
 
 fun main() = singleWindowApplication(title = "Context menu") {
-    val text = remember { mutableStateOf("Hello!") }
-    Column {
-        ContextMenuDataProvider(
-            items = {
-                listOf(
-                    ContextMenuItem("User-defined Action") {/*do something here*/ },
-                    ContextMenuItem("Another user-defined action") {/*do something else*/ }
-                )
-            }
-        ) {
-            TextField(
-                value = text.value,
-                onValueChange = { text.value = it },
-                label = { Text(text = "Input") }
-            )
-
-            Spacer(Modifier.height(16.dp))
-
-            SelectionContainer {
-                Text("Hello World!")
-            }
-        }
+    ContextMenuArea(items = {
+        listOf(
+            ContextMenuItem("User-defined Action") {/*do something here*/},
+            ContextMenuItem("Another user-defined action") {/*do something else*/}
+        )
+    }) {
+        Box(modifier = Modifier.background(Color.Blue).height(100.dp).width(100.dp))
     }
 }
