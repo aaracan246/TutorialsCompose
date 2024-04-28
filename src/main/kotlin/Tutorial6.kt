1f0806f50d2bccfc4ca979c78c0be9d77a857fc2
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 17:44:18 +0200

    Tuto6 1ra y 2nda parte

---
+++ src/main/kotlin/Tutorial6.kt
@@ -0,0 +1,48 @@
+//import androidx.compose.material.Button
+//import androidx.compose.material.Text
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.application
+//
+//fun main() = application {
+//    var fileName by remember { mutableStateOf("Untitled") }
+//
+//    Window(onCloseRequest = ::exitApplication, title = "$fileName - Editor") {
+//        Button(onClick = { fileName = "note.txt" }) {
+//            Text("Save")
+//        }
+//    }
+//}
+
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import kotlinx.coroutines.delay
+
+fun main() = application {
+    var isPerformingTask by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(2000) // Do some heavy lifting
+        isPerformingTask = false
+    }
+
+    if (isPerformingTask) {
+        Window(onCloseRequest = ::exitApplication) {
+            Text("Performing some tasks. Please wait!")
+        }
+    } else {
+        Window(onCloseRequest = ::exitApplication) {
+            Text("Hello, World!")
+        }
+    }
+}
