c3915bc73f2b49883403994c6d48b43c7f5b8f8b
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 21:10:02 +0200

    Tuto6 12d a parte

--- src/main/kotlin/Tutorial6.kt
+++ src/main/kotlin/Tutorial6.kt
@@ -439,3 +439,83 @@
 //    },
 //    dispose = FileDialog::dispose
 //)
+
+
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.window.WindowDraggableArea
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.application
+//
+//fun main() = application {
+//    Window(onCloseRequest = ::exitApplication, undecorated = true) {
+//        WindowDraggableArea {
+//            Box(Modifier.fillMaxWidth().height(48.dp).background(Color.DarkGray))
+//        }
+//    }
+//}
+
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.window.WindowDraggableArea
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.WindowScope
+//import androidx.compose.ui.window.application
+//
+//fun main() = application {
+//    Window(onCloseRequest = ::exitApplication, undecorated = true) {
+//        AppWindowTitleBar()
+//    }
+//}
+//
+//@Composable
+//private fun WindowScope.AppWindowTitleBar() = WindowDraggableArea {
+//    Box(Modifier.fillMaxWidth().height(48.dp).background(Color.DarkGray))
+//}
+
+
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material.Surface
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.shadow
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.application
+//import androidx.compose.material.Text
+//import androidx.compose.runtime.*
+//
+//fun main() = application {
+//    var isOpen by remember { mutableStateOf(true) }
+//    if (isOpen) {
+//        Window(
+//            onCloseRequest = { isOpen = false },
+//            title = "Transparent Window Example",
+//            transparent = true,
+//            undecorated = true, //transparent window must be undecorated
+//        ) {
+//            Surface(
+//                modifier = Modifier.fillMaxSize().padding(5.dp).shadow(3.dp, RoundedCornerShape(20.dp)),
+//                color = Color(55, 55, 55),
+//                shape = RoundedCornerShape(20.dp) //window has round corners now
+//            ) {
+//                Text("Hello World!", color = Color.White)
+//            }
+//        }
+//    }
+//}
