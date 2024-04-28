cb7c0ac63cd8538c5bcb787110915d505bfd430a
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 18:01:38 +0200

    Tuto6 3ra parte

--- src/main/kotlin/Tutorial6.kt
+++ src/main/kotlin/Tutorial6.kt
@@ -17,32 +17,67 @@
 //    }
 //}
 
+//
+//import androidx.compose.material.Text
+//import androidx.compose.runtime.LaunchedEffect
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.application
+//import kotlinx.coroutines.delay
+//
+//fun main() = application {
+//    var isPerformingTask by remember { mutableStateOf(true) }
+//
+//    LaunchedEffect(Unit) {
+//        delay(2000) // Do some heavy lifting
+//        isPerformingTask = false
+//    }
+//
+//    if (isPerformingTask) {
+//        Window(onCloseRequest = ::exitApplication) {
+//            Text("Performing some tasks. Please wait!")
+//        }
+//    } else {
+//        Window(onCloseRequest = ::exitApplication) {
+//            Text("Hello, World!")
+//        }
+//    }
+//}
 
+
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.DialogWindow
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.delay
 
 fun main() = application {
-    var isPerformingTask by remember { mutableStateOf(true) }
+    var isOpen by remember { mutableStateOf(true) }
+    var isAskingToClose by remember { mutableStateOf(false) }
 
-    LaunchedEffect(Unit) {
-        delay(2000) // Do some heavy lifting
-        isPerformingTask = false
-    }
-
-    if (isPerformingTask) {
-        Window(onCloseRequest = ::exitApplication) {
-            Text("Performing some tasks. Please wait!")
-        }
-    } else {
-        Window(onCloseRequest = ::exitApplication) {
-            Text("Hello, World!")
+    if (isOpen) {
+        Window(
+            onCloseRequest = { isAskingToClose = true }
+        ) {
+            if (isAskingToClose) {
+                DialogWindow(
+                    onCloseRequest = { isAskingToClose = false },
+                    title = "Close the document without saving?",
+                ) {
+                    Button(
+                        onClick = { isOpen = false }
+                    ) {
+                        Text("Yes")
+                    }
+                }
+            }
         }
     }
 }
