18bc16eec43748e47c36fe32bb71526cd1a438b0
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 21:44:33 +0200

    Tuto8 2da parte

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.window.application
 
 @Composable
 @Preview
-fun App() {
+fun App1() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
@@ -26,6 +26,6 @@ fun App() {
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        Tuto3_9()
+        //Tuto3_9()
     }
 }
