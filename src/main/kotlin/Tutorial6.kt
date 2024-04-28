ca679728be458bad4e46d6731de76ed62b7e9eee
aaracan246 <acalberto@iesrafaelalberti.es>
2024-04-28 18:06:44 +0200

    Tuto6 6ta parte

--- src/main/kotlin/Tutorial6.kt
+++ src/main/kotlin/Tutorial6.kt
@@ -137,67 +137,99 @@
 //}
 
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.window.ApplicationScope
-import androidx.compose.ui.window.MenuBar
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.key
+//import androidx.compose.runtime.mutableStateListOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.ui.window.ApplicationScope
+//import androidx.compose.ui.window.MenuBar
+//import androidx.compose.ui.window.Window
+//import androidx.compose.ui.window.application
+//
+//fun main() = application {
+//    val applicationState = remember { MyApplicationState() }
+//
+//    for (window in applicationState.windows) {
+//        key(window) {
+//            MyWindow(window)
+//        }
+//    }
+//}
+//
+//@Composable
+//private fun ApplicationScope.MyWindow(
+//    state: MyWindowState
+//) = Window(onCloseRequest = state::close, title = state.title) {
+//    MenuBar {
+//        Menu("File") {
+//            Item("New window", onClick = state.openNewWindow)
+//            Item("Exit", onClick = state.exit)
+//        }
+//    }
+//}
+//
+//private class MyApplicationState {
+//    val windows = mutableStateListOf<MyWindowState>()
+//
+//    init {
+//        windows += MyWindowState("Initial window")
+//    }
+//
+//    fun openNewWindow() {
+//        windows += MyWindowState("Window ${windows.size}")
+//    }
+//
+//    fun exit() {
+//        windows.clear()
+//    }
+//
+//    private fun MyWindowState(
+//        title: String
+//    ) = MyWindowState(
+//        title,
+//        openNewWindow = ::openNewWindow,
+//        exit = ::exit,
+//        windows::remove
+//    )
+//}
+//
+//private class MyWindowState(
+//    val title: String,
+//    val openNewWindow: () -> Unit,
+//    val exit: () -> Unit,
+//    private val close: (MyWindowState) -> Unit
+//) {
+//    fun close() = close(this)
+//}
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
-    val applicationState = remember { MyApplicationState() }
-
-    for (window in applicationState.windows) {
-        key(window) {
-            MyWindow(window)
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(width = Dp.Unspecified, height = Dp.Unspecified),
+        title = "Adaptive",
+        resizable = false
+    ) {
+        Column(Modifier.background(Color(0xFFEEEEEE))) {
+            Row {
+                Text("label 1", Modifier.size(100.dp, 100.dp).padding(10.dp).background(Color.White))
+                Text("label 2", Modifier.size(150.dp, 200.dp).padding(5.dp).background(Color.White))
+                Text("label 3", Modifier.size(200.dp, 300.dp).padding(25.dp).background(Color.White))
+            }
         }
     }
-}
-
-@Composable
-private fun ApplicationScope.MyWindow(
-    state: MyWindowState
-) = Window(onCloseRequest = state::close, title = state.title) {
-    MenuBar {
-        Menu("File") {
-            Item("New window", onClick = state.openNewWindow)
-            Item("Exit", onClick = state.exit)
-        }
-    }
-}
-
-private class MyApplicationState {
-    val windows = mutableStateListOf<MyWindowState>()
-
-    init {
-        windows += MyWindowState("Initial window")
-    }
-
-    fun openNewWindow() {
-        windows += MyWindowState("Window ${windows.size}")
-    }
-
-    fun exit() {
-        windows.clear()
-    }
-
-    private fun MyWindowState(
-        title: String
-    ) = MyWindowState(
-        title,
-        openNewWindow = ::openNewWindow,
-        exit = ::exit,
-        windows::remove
-    )
-}
-
-private class MyWindowState(
-    val title: String,
-    val openNewWindow: () -> Unit,
-    val exit: () -> Unit,
-    private val close: (MyWindowState) -> Unit
-) {
-    fun close() = close(this)
 }
